e7a03392634633cd42c19cf26981b2ac518f886e
adamant-antithesis <zt.gaming.sh@gmail.com>
2023-07-30 13:41:15 +0300

    Added user models, task and registered them

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True, blank=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.TextField(blank=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.email
+
+
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('New', 'New'),
+        ('Actual', 'Actual'),
+        ('Completed', 'Completed'),
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
