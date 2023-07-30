86abe3db2f42e38a474eaafa9c76a9e74e194d46
adamant-antithesis <zt.gaming.sh@gmail.com>
2023-07-30 13:59:30 +0300

    Added serializers

---
+++ tasks/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from .models import User, Task
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError('Username must be unique.')
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Email address must be unique.')
+        return value
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        first_name = validated_data['first_name']
+        last_name = validated_data.get('last_name', '')
+        email = validated_data['email']
+        password = validated_data['password']
+
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
+        return user
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'status', 'user']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['user'] = instance.user_id
+        return representation
